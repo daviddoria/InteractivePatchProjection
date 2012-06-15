24387207906a158805fb69749972a0ad74e2c80e
David Doria <daviddoria@gmail.com>
2012-06-15 16:01:28 -0400

    Before removing inverse.

--- InteractivePatchProjectionWidget.cpp
+++ InteractivePatchProjectionWidget.cpp
@@ -144,27 +144,29 @@ void InteractivePatchProjectionWidget::DisplayPatches()
 
   // Compute the projected patch
   Eigen::VectorXf vectorized = PatchProjection::VectorizePatch(this->Image.GetPointer(), this->SelectedRegion);
+  std::cout << "vectorized: " << vectorized << std::endl;
 
   unsigned int numberOfDimensionsToProjectTo = this->sldDimensions->value();
 
-  Eigen::VectorXf projectedVector =
-          EigenHelpers::DimensionalityReduction(vectorized, this->ProjectionMatrix, numberOfDimensionsToProjectTo);
-
-  // Even though the DimensionalityReduction function does this truncation internally, we need it here to use in the
-  // inverse projection
   Eigen::MatrixXf truncatedProjectionMatrix = EigenHelpers::TruncateColumns(this->ProjectionMatrix, numberOfDimensionsToProjectTo);
+
+  Eigen::VectorXf projectedVector = truncatedProjectionMatrix.transpose() * vectorized;
+  std::cout << "projectedVector: " << projectedVector << std::endl;
+
   //std::cout << "truncatedProjectionMatrix size: " << truncatedProjectionMatrix.rows() << " x " << truncatedProjectionMatrix.cols() << std::endl;
   //std::cout << truncatedProjectionMatrix << std::endl;
 
+  // This line is the bottleneck.
   Eigen::MatrixXf inverseProjectionMatrix = EigenHelpers::PseudoInverse(truncatedProjectionMatrix);
   //std::cout << "inverseProjectionMatrix size: " << inverseProjectionMatrix.rows() << " x " << inverseProjectionMatrix.cols() << std::endl;
   //std::cout << inverseProjectionMatrix << std::endl;
 
   Eigen::VectorXf unprojectedVector = inverseProjectionMatrix.transpose() * projectedVector;
+  std::cout << "unprojectedVector: " << unprojectedVector << std::endl;
 
   // Apply the inverse normalization transform
-  unprojectedVector = this->StandardDeviationVector.matrix().asDiagonal() * unprojectedVector;
-  unprojectedVector += this->MeanVector;
+  unprojectedVector = this->StandardDeviationVector.matrix().asDiagonal() * unprojectedVector; // Multiply by the standard devation
+  unprojectedVector += this->MeanVector; // Add the mean
 
   ImageType::Pointer projectedPatchImage = ImageType::New();
   PatchProjection::UnvectorizePatch(unprojectedVector, projectedPatchImage.GetPointer(), this->Image->GetNumberOfComponentsPerPixel());
@@ -260,10 +262,15 @@ void InteractivePatchProjectionWidget::OpenImage(const std::string& fileName)
   std::cout << "Computing projection matrix with radius = " << GetPatchRadius() << std::endl;
   // NOTE: this will crash if the patch size is too big (too big for RAM in a machine with 4GB).
   // Known to work with radius=7, known to not work with radius=15
-//   this->ProjectionMatrix = PatchProjection::ComputeProjectionMatrix(this->Image.GetPointer(), GetPatchRadius(),
-//                                                                     this->MeanVector, this->StandardDeviationVector);
-  this->ProjectionMatrix = PatchProjection::GetDummyProjectionMatrix(this->Image.GetPointer(), GetPatchRadius(),
-                                                                     this->MeanVector, this->StandardDeviationVector);
+  this->ProjectionMatrix = PatchProjection::ComputeProjectionMatrix(this->Image.GetPointer(), GetPatchRadius(),
+                                                                    this->MeanVector, this->StandardDeviationVector);
+
+//   this->ProjectionMatrix = PatchProjection::GetDummyProjectionMatrix(this->Image.GetPointer(), GetPatchRadius(),
+//                                                                      this->MeanVector, this->StandardDeviationVector);
+
+  std::cout << "Mean: " << this->MeanVector << std::endl;
+  std::cout << "Standard deviation: " << this->StandardDeviationVector << std::endl;
+  std::cout << "Projection matrix is " << this->ProjectionMatrix.rows() << " x " << this->ProjectionMatrix.cols() << std::endl;
 
   this->sldDimensions->setMinimum(1);
   this->sldDimensions->setMaximum(this->ProjectionMatrix.rows());
